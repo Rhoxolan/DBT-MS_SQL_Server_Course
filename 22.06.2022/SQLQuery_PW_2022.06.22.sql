d5751377fd89350123cddcc7e266156ae96f522b
Pavel Batsemakin <98427038+Rhoxolan@users.noreply.github.com>
2022-07-15 10:10:40 +0300

    Update SQLQuery_PW_2022.06.22.sql

--- 22.06.2022/SQLQuery_PW_2022.06.22.sql
+++ 22.06.2022/SQLQuery_PW_2022.06.22.sql
@@ -28,3 +28,61 @@ Insert Professors VALUES
 ((SELECT Id FROM Doctors WHERE Name = 'Василий Павлович')),
 ((SELECT Id FROM Doctors WHERE Name = 'Петр Васильевич')),
 ((SELECT Id FROM Doctors WHERE Name = 'Анатолий Анатольевич'))
+
+ALTER TABLE DoctorsExaminations
+ADD DateExam DATE NOT NULL DEFAULT '2022-01-01'
+
+--1. Вывести названия и вместимости палат, расположенных в 5-м корпусе, вместимостью 5 и более мест,
+-- если в этом корпусе есть хотя бы одна палата вместимостью более 15 мест.
+-- "EXISTS"
+--Вариант 1
+SELECT Wards.Name, Wards.Places
+FROM Wards, Departments
+WHERE Wards.DepartmentID = Departments.Id AND EXISTS (SELECT * FROM Wards WHERE Wards.DepartmentID = Departments.Id AND Wards.Places > 15) AND
+Wards.Places > 5  AND Departments.Building = 5
+--Вариант 2
+SELECT Wards.Name, Wards.Places
+FROM Wards
+WHERE EXISTS (SELECT * FROM Departments WHERE Wards.DepartmentID = Departments.Id AND Wards.Places > 15 AND Departments.Building = 5) AND
+Wards.Places > 5
+
+--2. Вывести названия отделений в которых проводилось хотя бы одно обследование за последнюю неделю
+-- "INNER JOIN"
+SELECT Departments.Name, DoctorsExaminations.DateExam
+FROM Departments
+JOIN Wards ON Wards.DepartmentID = Departments.Id
+JOIN DoctorsExaminations ON DoctorsExaminations.WardId = Wards.Id
+WHERE DATEDIFF(day, DoctorsExaminations.DateExam, GETDATE()) < 7
+
+--3. Вывести названия заболеваний, для которых не проводятся обследования.
+-- "LEFT JOIN"
+-- Выводим все строки, если обследования нет - DE.DiseasID NULL
+SELECT Diseases.Name, DoctorsExaminations.DiseasID
+FROM Diseases LEFT JOIN DoctorsExaminations ON DoctorsExaminations.DiseasID = Diseases.Id
+
+--4. Вывести полные имена врачей, которые не проводят обследования.
+-- "NOT EXISTS"
+SELECT Doctors.Name
+FROM Doctors
+WHERE NOT EXISTS (SELECT * FROM DoctorsExaminations WHERE Doctors.Id = DoctorsExaminations.DoctorId)
+
+--5. Вывести названия отделений, в которых не проводятся обследования.
+-- "NOT EXISTS"
+SELECT Departments.Name
+FROM Departments
+WHERE NOT EXISTS (SELECT * FROM DoctorsExaminations, Wards WHERE Wards.DepartmentID = Departments.Id AND DoctorsExaminations.WardId = Wards.Id)
+
+--6. Вывести фамилии врачей, которые являются интернами
+-- "INNER JOIN"
+SELECT Doctors.Name
+FROM Doctors
+JOIN Interns ON Interns.DoctorId = Doctors.Id
+
+--7. Вывести фамилии интернов, ставки которых больше, чем ставка хотя бы одного из врачей.
+-- "INNER JOIN", "NOT EXISTS"
+SELECT Doctors.Name
+FROM Doctors
+JOIN Interns ON Interns.DoctorId = Doctors.Id
+WHERE Doctors.Salary > ANY (SELECT Doctors.Salary FROM Doctors WHERE NOT EXISTS (SELECT * FROM Interns WHERE Doctors.Id = Interns.DoctorId))
+
+--8. Вывести названия палат, чья вместимость больше, чем вместимость каждой палаты, находящейся в 3-м корпусе.
