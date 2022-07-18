f20768b022263bf22e119f6ea69059e578149b0a
Pavel Batsemakin <98427038+Rhoxolan@users.noreply.github.com>
2022-07-18 10:30:02 +0300

    Update SQLQuery_PW_2022.06.23.sql

--- 23.06.2022/SQLQuery_PW_2022.06.23.sql
+++ 23.06.2022/SQLQuery_PW_2022.06.23.sql
@@ -13,6 +13,12 @@ Price INT NOT NULL,
 Manufacturer INT NOT NULL
 )
 
+CREATE TABLE Archive
+(
+Id INT PRIMARY KEY IDENTITY NOT NULL,
+ProductId INT REFERENCES Products(Id) NOT NULL
+)
+
 CREATE TABLE Categories
 (
 Id INT PRIMARY KEY IDENTITY NOT NULL,
@@ -26,6 +32,12 @@ ProductId INT REFERENCES Products(Id) ON DELETE CASCADE NOT NULL,
 SellingDate DATE CHECK(SellingDate > '2010-01-01' AND SellingDate <= GETDATE()) DEFAULT GETDATE() NOT NULL,
 SalesmanId INT REFERENCES Salesmans(Id) NOT NULL,
 BuyerId INT REFERENCES Buyers(Id) NULL
+)
+
+CREATE TABLE History
+(
+Id INT PRIMARY KEY IDENTITY NOT NULL,
+SellingId INT REFERENCES Sellings(Id) NOT NULL
 )
 
 CREATE TABLE Salesmans
@@ -53,3 +65,93 @@ Phone NVARCHAR(100) CHECK(Phone != '') NULL,
 Gender NVARCHAR(100) CHECK(Gender != '') NOT NULL,
 IsSubs BIT DEFAULT 0 NOT NULL
 )
+
+ALTER TABLE Buyers
+ADD SailPercent INT CHECK(SailPercent > -1 AND SailPercent < 101) NOT NULL DEFAULT 0
+
+--1. При продаже товара, заносить информацию о продаже в таблицу «История». Таблица «История» используется для дубляжа информации о всех продажах
+CREATE TRIGGER Sellings_INSERT
+ON Sellings
+AFTER INSERT
+AS
+INSERT INTO History
+SELECT Id FROM inserted
+
+--2. Если после продажи товара не осталось ни одной единицы данного товара, необходимо перенести информацию
+-- о полностью проданном товаре в таблицу «Архив»
+CREATE TRIGGER Sellings_INSERT_2
+ON Sellings
+AFTER INSERT
+AS
+IF (SELECT ProductsAmount FROM Products JOIN inserted ON inserted.ProductId = Products.Id) = 0
+	BEGIN
+	INSERT Archive VALUES
+	((SELECT inserted.ProductId FROM inserted))
+	END;
+
+--3. Не позволять регистрировать уже существующего клиента. При вставке проверять наличие клиента по номеру и email
+-- https://dotnettutorials.net/lesson/instead-of-insert-trigger-in-sql-server/
+CREATE TRIGGER Buyers_INSERT
+ON Buyers
+INSTEAD OF INSERT
+AS
+IF EXISTS (SELECT Buyers.Email, Buyers.Phone FROM Buyers JOIN inserted ON Buyers.Email = inserted.Email OR Buyers.Phone = inserted.Phone)
+	BEGIN
+	raiserror('Пользователь с такими данными уже зарегестрирован',16,1)
+	RETURN
+	END;
+ELSE
+	BEGIN
+	INSERT INTO Buyers(Id, FullName, Email, Phone, Gender, IsSubs)
+	SELECT Id, FullName, Email, Phone, Gender, IsSubs
+	FROM inserted
+END;
+
+--4. Запретить удаление существующих клиентов
+CREATE TRIGGER Buyers_DELETE
+ON Buyers
+INSTEAD OF DELETE
+AS
+	BEGIN
+	raiserror('Мы не можем удалять зарегестрированного клиента!',16,1)
+	RETURN
+	END
+
+--5. Запретить удаление сотрудников, принятых на работу до 2015 года
+CREATE TRIGGER Salesmans_DELETE
+ON Salesmans
+INSTEAD OF DELETE
+AS
+IF (SELECT deleted.StartDate FROM deleted)  < '2015-01-01'
+	BEGIN
+	raiserror('Мы не можем удалить этого сотрудника!',16,1)
+	RETURN
+	END;
+ELSE
+	BEGIN
+	DELETE FROM Salesmans
+	WHERE Salesmans.Id = (SELECT deleted.Id from deleted)
+	END;
+
+--6. При новой покупке товара нужно проверять общую сумму покупок клиента.
+-- Если сумма превысила 50000 грн, необходимо установить процент скидки в 15%
+CREATE TRIGGER Sellings_INSERT_3
+ON Sellings
+AFTER INSERT
+AS
+	BEGIN
+	DECLARE @buyrId INT;
+	SET @buyrId = (SELECT inserted.BuyerId FROM inserted)
+	IF (@buyrId is not null)
+		BEGIN
+		IF (SELECT SUM(Products.Price)
+			FROM Products
+			JOIN inserted ON inserted.ProductId = Products.Id
+			JOIN Buyers ON Buyers.Id = inserted.BuyerId) > 50000
+				BEGIN
+				UPDATE Buyers
+				SET SailPercent = 15
+				WHERE Buyers.Id = @buyrId
+				END;
+		END;
+	END;
